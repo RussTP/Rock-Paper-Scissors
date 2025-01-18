4a139cdb5a342fa10f1785d42b81f3b18cc9f610
RussTP <rpawlett@live.com>
2025-01-18 12:12:44 -0500

    fixed functionality with rock paper scissors buttons, disabled buttons after round 5 to prevent continuous play, play again button appears when round is over

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -35,30 +35,46 @@ else if (
     }
 }
 
-const buttons = document.querySelectorAll(".choice-btn");
+const buttons = document.querySelectorAll(".choiceBtn");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const humanChoice = button.id;
         const computerChoice = getComputerChoice();
-       const result = playRound(humanChoice, computerChoice);
+        const result = playRound(humanChoice, computerChoice);
 
     const resultContainer = document.querySelector("#resultContainer");   
-    resultContainer.textContent = result;
+        resultContainer.textContent = result;
 
     const scoreDiv = document.querySelector("#runningScore");
-    scoreDiv.textContent = `Score: Human ${humanScore} - Computer ${computerScore}`;
+        scoreDiv.textContent = `Score: Human ${humanScore} - Computer ${computerScore}`;
+       
 
     if (humanScore === 5 || computerScore === 5) {
-    const winner = humanScore > computerScore ? "You" : "Computer";
-    document.querySelector("#runningScore");
-    runningScore.textContent = `${winner} won the game! Final Score ${humanScore} - Computer ${computerScore}`;
+        const winner = humanScore > computerScore ? "You" : "Computer";
+        scoreDiv.textContent = `${winner} won the game! Final Score - You ${humanScore} - Computer ${computerScore}`;
+        buttons.forEach((btn) => (btn.disabled =true));
+    
+
+    const btnDiv = document.getElementById("buttonContainer")
+    const newButton = document.createElement("button");
+    newButton.innerText = "Play again!"
+    newButton.id = "reset"
+    btnDiv.appendChild(newButton); 
+    newButton.addEventListener("click", resetGame);
+
+        }
+    })
+});  
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
    
-    }
-    });
-});
+    document.querySelector("#resultContainer").textContent = "";
+    document.querySelector("#runningScore").textContent = "Score: You 0 - Computer 0";
 
+    buttons.forEach((btn) => (btn.disabled = false));
 
-
-
-
-
+    const resetButton = document.getElementById("reset");
+    resetButton.remove();
+};
